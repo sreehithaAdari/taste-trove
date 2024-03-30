5a4d79da9dfe9469d326df89026321550896e44e
Sreehitha Adari <happysiri003@gmail.com>
2024-03-30 07:47:34 +0530

    recipie-finder

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className='header-content flex align-center justify-center flex-column text-center'>
         <SearchForm />
         <h1 className='text-white header-title ls-2'>What are your favorite cuisines?</h1>
-        <p className='text-uppercase text-white my-3 ls-1'>personalize your experience</p>
+        <p className='text-uppercase text-white my-3 ls-1'>Personalize your experience</p>
       </div>
     </header>
   )
